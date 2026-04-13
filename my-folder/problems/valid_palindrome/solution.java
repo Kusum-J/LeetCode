e61c477a7bc28dec1e3a71785a57afb682a4e94a
Kusum Jorapur <jorapurkusum@gmail.com>
2026-04-13 09:18:15 +0000

    Sync LeetCode submission - Valid Palindrome (java)

---
+++ my-folder/problems/valid_palindrome/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) 
+    {
+        int left = 0;
+        int ryt = s.length() - 1;
+
+        while(left < ryt)
+        {
+            while(left < ryt && !Character.isLetterOrDigit(s.charAt(left)))
+            {
+                left++;
+            }
+
+            while(left < ryt && !Character.isLetterOrDigit(s.charAt(ryt)))
+            {
+                ryt--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(ryt)))
+            {
+                return false;
+            }
+            left++;
+            ryt--;
+        }
+
+        return true;
+    }
+}
