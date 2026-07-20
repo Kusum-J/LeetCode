ce36194ff6074ea50ce41e91374d37c6b80d30ea
Kusum Jorapur <jorapurkusum@gmail.com>
2026-07-20 06:41:14 +0000

    Sync LeetCode submission - Reverse Words in a String (java)

--- my-folder/problems/reverse_words_in_a_string/solution.java
+++ my-folder/problems/reverse_words_in_a_string/solution.java
@@ -1,7 +1,11 @@
 class Solution {
     public String reverseWords(String s) 
     {
-        String[] words = s.trim().split("\\s+");
+        // \s+ : treats all the separate whitespaces as a single character.
+        // \s : any space character : tab, newline, etc
+        // + : combines all of them
+
+        String[] words = s.trim().split("\s+");
         StringBuilder result = new StringBuilder();
         
         for (int i = words.length - 1; i >= 0; i--) 
