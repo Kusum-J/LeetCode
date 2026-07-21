4a4a1107347b5b54a1f0d90405888971e6b572eb
Kusum Jorapur <jorapurkusum@gmail.com>
2026-07-21 15:43:07 +0000

    Sync LeetCode submission - Longest Palindromic Substring (java)

--- my-folder/problems/longest_palindromic_substring/solution.java
+++ my-folder/problems/longest_palindromic_substring/solution.java
@@ -1,26 +1,35 @@
-class Solution {
-    public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-        int start = 0, end = 0;
+class Solution 
+{
+    int start = 0;
+    int maxLen = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);       // Odd length
-            int len2 = expandAroundCenter(s, i, i + 1);   // Even length
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
+    public String longestPalindrome(String s) 
+    {
+        if(s == null || s.length() == 0)
+            return s;
+
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            expand(s, i, i); //odd
+            expand(s, i, i+1);
         }
 
-        return s.substring(start, end + 1);
+        return s.substring(start, start + maxLen);
     }
 
-    private int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+    private void expand(String s, int left, int ryt)
+    {
+        while(left >= 0 && ryt < s.length() && s.charAt(left) == s.charAt(ryt) )
+        {
             left--;
-            right++;
+            ryt++;
         }
-        return right - left - 1;
+        int len = ryt - left - 1;
+
+        if(len > maxLen)
+        {
+            maxLen = len;
+            start = left + 1;
+        }
     }
 }
