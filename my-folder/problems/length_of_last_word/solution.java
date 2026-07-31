52909e0e5e0ba8f2b0cbff4a3cc8465b3ea3ac4e
Kusum Jorapur <jorapurkusum@gmail.com>
2026-07-31 17:09:30 +0000

    Sync LeetCode submission - Length of Last Word (java)

--- my-folder/problems/length_of_last_word/solution.java
+++ my-folder/problems/length_of_last_word/solution.java
@@ -2,6 +2,7 @@ class Solution {
     public int lengthOfLastWord(String s) 
     {
         String[] words = s.trim().split(" ");
+        
         return words[words.length - 1].length();
     }
 }
